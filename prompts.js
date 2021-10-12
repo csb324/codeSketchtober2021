d78b839aaad472c857d230e1b0bd30a4d6d67075
Clara Beyer <csb324@gmail.com>
2021-10-12 17:22:51 -0400

    small crimes

--- prompts.js
+++ prompts.js
@@ -10,7 +10,7 @@ const prompts = {
   "day9": { "name": "Heartbeat", "file": "day9" }, 
   "day10": { "name": "Games are art and you cannot let anyone tell you otherwise", "file": "day10" }, 
   "day11": { "name": "Elemental", "file": "day11" }, 
-  "day12": { "name": "Be small, do crimes", "file": "day12" }, 
+  "day12": { "name": "Be small, do crimes", "file": "day12", thumbnail: true }, 
   "day13": { "name": "A chain of blocks but NOT blockchain okay? Idk man figure it out", "file": "day13" }, 
   "day14": { "name": "Under construction", "file": "day14" }, 
   "day15": { "name": "Delicious", "file": "day15" }, 
--- src/pieces/day12.js
+++ src/pieces/day12.js
@@ -1,23 +1,101 @@
 import p5 from 'p5';
 import * as utils from '../utils';
 
+let r;
+let t = 0;
+
+class SmallBoi {
+  constructor(startingAngle, radius, criminal = false) {
+    this.criminal = criminal;
+    this.angle = startingAngle;
+    this.radius = randomGaussian(radius, 10);
+    this.atTimes = {};
+    this.criminal = criminal;
+
+    if(criminal) {
+      this.xSeed = random(2*PI);
+      this.ySeed = random(2*PI);
+      this.counter = 0;
+      this.setFactors();
+    }
+  }
+
+  draw(time) {
+    if(this.criminal) {
+      this.drawCrime(time);      
+    } else {
+      this.drawNormal(time);
+    }
+  }
+
+  setFactors() {
+    this.xFactor = ceil(noise(this.counter/4) * 4);
+    this.yFactor = ceil(noise(10, this.counter/4) * 4);  
+  }
+
+  drawCrime(time) {
+
+    if(time == 0) {
+      this.counter++
+      this.setFactors();
+    } 
+
+    const v = createVector(0, this.radius * 0.7);
+    v.rotate(this.angle + time);
+    // fill('green');
+    rect(v.x * (Math.cos(time*this.xFactor) + 1), v.y * (Math.sin(time*this.yFactor) + 1), utils.relSize(4), utils.relSize(4));
+  }
+
+  drawNormal(time) {
+    const cached = this.atTimes[time];
+    // fill(0);
+    if(!cached) {
+      const v = createVector(0, this.radius);
+      v.rotate(this.angle + time);
+      this.atTimes[time] = v;
+    }
+
+    rect(this.atTimes[time].x, this.atTimes[time].y, utils.relSize(4), utils.relSize(4));
+  }
+}
+
+let bois = [];
+let bgColor;
+
 function setup() {  
   utils.standardCanvas();
+  r = utils.relSize(300);
+
+  // noLoop();
+  bgColor = color(255, 255, 255, 40);
+  frameRate(60);
   reset();
-  noLoop();
+
 }
 
 function reset() {
-  fill(random(55) + 100);
+  background(255);
 
+  noStroke();
+  fill(0);  
+  bois = [];
+  bois.push(new SmallBoi(0, r, true));
+  for (let index = 0.1; index < 2*PI; index+= 0.1) {
+    bois.push(new SmallBoi(index, r));
+  }
 }
 
 function draw() {
-  rect(0, 0, width, height);
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  text("coming soon", width/2, height/2);
+  background(bgColor);
+
+  translate(width/2, height/2);
+  t += 0.02;
+
+  if(t > 2*PI) {
+    t = 0;
+  }
+
+  bois.forEach((b) => b.draw(t));
 }
 
 
