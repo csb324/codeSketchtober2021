a011c6ddb8db3ba5425c0d04f8d3b3f51a29011f
Clara Beyer <csb324@gmail.com>
2021-10-13 14:35:29 -0400

    commit to loudness

--- src/pieces/day11.js
+++ src/pieces/day11.js
@@ -4,58 +4,20 @@ import * as utils from '../utils';
 function setup() {  
   utils.standardCanvas();
   reset();
-  // noLoop();
+  noLoop();
 }
-
-let seed;
 
 function reset() {
-  noFill();
-  stroke(255);
-  seed = random(10000);
-}
+  fill(random(55) + 100);
 
-const increment = 30;
-
-function drawCurve(i) {
-  // noStroke();
-  beginShape();
-  // fill(random(0, 14), random(0, 100), random(100, 255));
-  for (let index = -200; index <= 1200; index+= increment) {
-    const x = index + Math.sin((frameCount + i) / 60) * 150;
-    const yNoise = (noise(
-      (index + frameCount)/40
-    )+ 1.5);
-    
-    let yWobble = Math.sin((600 - index)/random(120, 200));
-    yWobble = map(yWobble, -1, 1, 78, 80);
-
-    const yMod = noise(index / 100, i / 600) 
-      * yNoise 
-      * yWobble
-
-    const y = i - yMod;
-
-
-    curveVertex(
-      utils.relSize(x), 
-      utils.relSize(y)
-    );    
-  }
-
-  vertex(width, height * 1.5);
-  vertex(0, height * 1.5);
-  endShape(CLOSE);
 }
 
 function draw() {
-  background(0);
-  randomSeed(seed);
-
-  for (let index = 0; index < 1500; index += increment) {
-
-    drawCurve(index);    
-  }
+  rect(0, 0, width, height);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(20);
+  text("coming soon", width/2, height/2);
 }
 
 
--- src/pieces/day6.js
+++ src/pieces/day6.js
@@ -4,20 +4,69 @@ import * as utils from '../utils';
 function setup() {  
   utils.standardCanvas();
   reset();
-  noLoop();
+  // noLoop();
 }
 
+let seed;
+let colors;
 function reset() {
-  fill(random(55) + 100);
+  noFill();
+  stroke(255);
+  seed = random(10000);
+  // noLoop();
 
+  colors = [
+    "#ffff00",
+    "#ff00ff",
+    "#00ffff"
+  ];
+}
+
+const increment = 10;
+
+function drawCurve(i) {
+  // noStroke();
+  beginShape();
+  strokeWeight(utils.relSize(random(1, 5)))
+  stroke(random(colors));
+  // fill(random(0, 14), random(0, 100), random(100, 255));
+  for (let index = -200; index <= 1200; index+= (3*increment)) {
+    const x = index + Math.sin(i / 60);
+    const yNoise = (noise(
+      (index + frameCount)/40
+    )+ 1.5);
+    
+    let yWobble = Math.sin((600 - index)/random(120, 200));
+    yWobble = map(yWobble, -1, 1, 78, 80);
+
+    const yMod = noise(index / 100, i / 600) 
+      * yNoise 
+      * yWobble
+
+    const y = i - yMod;
+
+
+    curveVertex(
+      utils.relSize(x), 
+      utils.relSize(y)
+    );    
+  }
+
+  vertex(width, height * 1.5);
+  vertex(0, height * 1.5);
+  endShape(CLOSE);
 }
 
 function draw() {
-  rect(0, 0, width, height);
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textSize(20);
-  text("coming soon", width/2, height/2);
+  background(0);
+  randomSeed(seed);
+
+  for (let index = 0; index < 1500; index += increment) {
+    if(random() > (mouseX / width)) {
+      drawCurve(index);
+
+    }
+  }
 }
 
 
